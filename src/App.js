7a5d1a5728b605ddf65c3b2dae9a571aa0071630
An <nguyentruongan28074@gmail.com>
2023-02-21 14:27:24 +0700

    abc

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import Narbar from './components/Narbar/Narbar';
 import Carts from './components/carts/Carts';
 import Form from './components/form/Form';
+import Newfim from './components/newfim/Newfim';
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 function App() {
   const [dataCart, setDataCart] = useState([])
+  const [tudoCart, setTudoCart] = useState([])
   useEffect(() => {
     axios
       .get("http://localhost:3000/listproduct")
       .then((dataListProduct) => setDataCart(dataListProduct.data))
+      .catch((err) => console.log(err));
+  }, []);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/listproduct")
+      .then((dataListProduct) => setTudoCart(dataListProduct.data))
       .catch((err) => console.log(err));
   }, []);
   return (  
@@ -18,6 +26,9 @@ function App() {
   <Narbar/>
   <Carts
    dataCart={dataCart}/>
+   <Newfim
+  tudoCart={tudoCart}
+  />
   <Form/>
     </>
   );
---
+++ src/components/newfim/Newfim.jsx
@@ -0,0 +1,19 @@
+import "./newfim.css";
+import React from "react";
+
+function Newfim(props) {
+    let {
+        tudoCart,
+    } = props;
+    return ( 
+        <>
+        <div className="tudutong">
+            <div className="expected">
+                <p>New items</p>
+            </div>
+        </div>
+        </>
+     );
+}
+
+export default Newfim;
